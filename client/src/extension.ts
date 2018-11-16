4b6dfe217d84c3c995f641b8d20ee7f40bc03539
Sacha-Elie Ayoun <sacha-elie.ayoun@supelec.fr>
2018-11-16 17:58:08 +0000

    Use implementation in full OCaml of the server

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -4,37 +4,25 @@
  * ------------------------------------------------------------------------------------------ */
 'use strict';
 
-import * as path from 'path';
-import { workspace, ExtensionContext } from 'vscode';
+import { workspace } from 'vscode';
 
 import {
 	LanguageClient,
 	LanguageClientOptions,
-	ServerOptions,
-	TransportKind
+	ServerOptions
 } from 'vscode-languageclient';
 
 let client: LanguageClient;
 
-export function activate(context: ExtensionContext) {
+export function activate() {
 	// The server is implemented in node
-	let serverModule = context.asAbsolutePath(
-		path.join('server', 'out', 'server.js')
-	);
-	// The debug options for the server
-	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
-
+	let binaryLocation:string = workspace.getConfiguration('lsp').get('binaryPath')
 	// If the extension is launched in debug mode then the debug server options are used
 	// Otherwise the run options are used
 	let serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
-		debug: {
-			module: serverModule,
-			transport: TransportKind.ipc,
-			options: debugOptions
-		}
-	};
+					command: binaryLocation,
+					args: []
+			};
 
 	// Options to control the language client
 	let clientOptions: LanguageClientOptions = {
